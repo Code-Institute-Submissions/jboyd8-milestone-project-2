c626f7c4a44faa4ec37b54d057a01c55ae9ba8ef
jboyd8 <jamieboyd8@yahoo.com>
2019-08-21 20:10:49 +0100

    feat: added style to rotate x axis headings and second chart

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ function makeGraphs(error, data) {
     var ndx = crossfilter(data);
 
     showAttacksByYear(ndx);
+    showAttacksByCountry(ndx);
+    showAttacksByReason(ndx)
 
     dc.renderAll();
 }
@@ -20,7 +22,7 @@ function showAttacksByYear(ndx) {
     var group = dim.group();
 
     dc.barChart('#chart-one')
-        .width(1000)
+        .width(1200)
         .height(300)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(dim)
@@ -28,7 +30,34 @@ function showAttacksByYear(ndx) {
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .elasticY(true)
         .xAxisLabel('Year')
         .yAxis().ticks(20)
+}
+
+function showAttacksByCountry(ndx) {
+    var dim = ndx.dimension(dc.pluck('Country'));
+    var group = dim.group();
+
+    dc.barChart('#chart-three')
+        .width(1500)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel('Country')
+        .yAxis().ticks(20)
+}
+
+function showAttacksByReason(ndx) {
+    var dim = ndx.dimension(dc.pluck('Type'));
+    var group = dim.group();
+
+    dc.pieChart
+        .height(300)
+        .radius(300)
+        .dimension(dim)
+        .group(group)
 }
