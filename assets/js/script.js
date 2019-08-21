24f4660db372e07d997e1196e6f576fb0c7dfa1f
jboyd8 <jamieboyd8@yahoo.com>
2019-08-21 19:43:23 +0100

    chore: added first successful graph.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,33 @@ queue()
     .defer(d3.csv, "assets/data/data.csv")
     .await(makeGraphs);
 
-//Create variables for each chart
-
-var attacksByYear = dc.barChart('#chart-one')
-var attacksByCountry = dc.barChart('chart-two')
 
 
 
-//Convert CSV file into readable format
-function makeGraphs(data) {
-    
-    console.log(data)
-    // var ndx = crossfilter(data);
 
-    // var yearDim = ndx.dimension(dc.pluck('Year'));
-    // var yearGroup = yearDim.group();
 
-    // attacksByYear
-    //     .width(300)
-    //     .height(1000)
-    //     .dimension(yearDim)
-    //     .group(yearGroup)
-    //     .x(d3.scale.linear().domain(yearDim))
-    //     .xUnits(dc.units.linear)
-    //     .xAxisLabel("Year")
-    //     .yAxisLabel("Count")
-    //     .brushOn(false)
+function makeGraphs(error, data) {
+    var ndx = crossfilter(data);
 
-    // dc.renderAll();
+    showAttacksByYear(ndx);
+
+    dc.renderAll();
+}
+
+function showAttacksByYear(ndx) {
+    var dim = ndx.dimension(dc.pluck('Year'));
+    var group = dim.group();
+
+    dc.barChart('#chart-one')
+        .width(1000)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel('Year')
+        .yAxis().ticks(20)
 }
